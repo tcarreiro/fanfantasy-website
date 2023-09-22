996554813f0dc49dcd2c952ec729e35d4c450354
tcarreiro <thiago.carreiro@hotmail.com>
2023-09-22 07:09:07 +0000

    .env test

--- website/__init__.py
+++ website/__init__.py
@@ -12,8 +12,7 @@ DB_NAME = 'database.db'
 
 def create_app():
   app = Flask(__name__)
-  app.config[
-      'SECRET_KEY'] = 'aiusdiuhasdiuhasiudhaiuhsdiuhasd'  #os.getenv("SECRET_KEY")
+  app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
   app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
   db.init_app(app)
 
