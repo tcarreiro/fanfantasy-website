0f6d014fc5f03da4d35ab9ff2668dd70cbe014f6
tcarreiro <thiago.carreiro@hotmail.com>
2023-09-22 06:39:36 +0000

    Same as vscode, except for .env
    Seems like .env doesn't work on repl.it

---
+++ website/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
+
+
+def create_app():
+  app = Flask(__name__)
+  app.config[
+      'SECRET_KEY'] = 'aiusdiuhasdiuhasiudhaiuhsdiuhasd'  #os.getenv("SECRET_KEY")
+  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+  db.init_app(app)
+
+  from .views import views
+  from .auth import auth
+
+  app.register_blueprint(views, url_prefix='/')
+  app.register_blueprint(auth, url_prefix='/')
+
+  from .models import User, Note
+
+  with app.app_context():
+    db.create_all()
+
+  return app
